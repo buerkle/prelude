fc6c9905176becb45e3379c594cb4c7056a2a3ae
Richie Jefts <richard.jefts@drakerenergy.com>
2015-06-02 09:59:17 -0500

    Fix comment-region and uncomment-region keybindings

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -1,7 +1,11 @@
 (global-set-key (kbd "C-c c") 'kill-this-buffer)
 
-(global-set-key (kbd "C-c s") 'comment-region)
+
+(global-set-key (kbd "C-M-\\") 'prelude-cleanup-buffer-or-region) 
+
+(define-key prelude-mode-map "\C-ce" nil)
 (global-set-key (kbd "C-c e") 'uncomment-region)
 
-(global-set-key (kbd "C-M-\\") 'prelude-cleanup-buffer-or-region)
+(define-key prelude-mode-map "\C-cs" nil)
+(global-set-key (kbd "C-c s") 'comment-region)
 
